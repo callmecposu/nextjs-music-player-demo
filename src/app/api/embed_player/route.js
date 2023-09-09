c19acbd710bfc19c84cc5b77c087c92626e57fc9
callmecposu <pc.cposu@gmail.com>
2023-09-09 15:43:19 -0400

    added error message whem oembed fails to get the embed code for soundcloud player

--- src/app/api/embed_player/route.js
+++ src/app/api/embed_player/route.js
@@ -2,15 +2,20 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   let trackURL = new URL(request.url).searchParams.get("url");
-  trackURL = 'https://soundcloud.com' + trackURL;
+  trackURL = "https://soundcloud.com" + trackURL;
   console.log("track URL: ", trackURL);
   const oembedResp = await fetch(
     `https://soundcloud.com/oembed?format=json&url=${trackURL}`
   );
-  const oembedRes = await oembedResp.json();
-  const embedCode = oembedRes.html.replace(
-    '"></iframe>',
-    '&show_comments=false&hide_related=true&show_reposts=false&show_teaser=false"></iframe>'
-  );
-  return NextResponse.json({ embedCode }, { status: 200 });
+  if (oembedResp.ok) {
+    const oembedRes = await oembedResp.json();
+    console.log(oembedRes);
+    const embedCode = oembedRes.html.replace(
+      '"></iframe>',
+      '&show_comments=false&hide_related=true&show_reposts=false&show_teaser=false"></iframe>'
+    );
+    return NextResponse.json({ embedCode }, { status: 200 });
+  } else {
+    return NextResponse.json({embedCode: 'failed to get the embed code'}, {status:oembedResp.status})
+  }
 }
